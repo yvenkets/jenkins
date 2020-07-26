59d0a47efb0aa92d8a178995a0e03d1bce76fb62
Venkertraman <yvenkets@gmail.com>
2020-07-26 22:39:48 +0530

    Create instal.sh

---
+++ instal.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo su
+wget -q -O — https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
+sudo apt-get install jenkins
