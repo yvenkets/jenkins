087f98b0a754404791ff3d3e023acc89be824691
Venkertraman <yvenkets@gmail.com>
2020-07-28 20:57:35 +0530

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo su
-wget -q -O — https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
-echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins
